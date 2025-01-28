3d7af19fe2629d4ac63ff77646b937153611bea5
teonteon19 <92378359+teonteon19@users.noreply.github.com>
2025-01-28 12:57:51 +0800

    fix script.js error

--- script.js
+++ script.js
@@ -31,28 +31,19 @@ class Subcontent{
         div.className='thumbnail';
         div.id=this.name;
 
-        let title=this.createTitleDiv();
-        div.appendChild(title)
-
-        let imgDiv=this.createImgDiv();
-        div.appendChild(this.createImgDiv)
-
-        contentDiv.appendChild(div)
-    }
-
-    createTitleDiv(){
         let title=document.createElement('div');
         title.innerText=this.name
-        return title
-    }
+        div.appendChild(title)
 
-    createImgDiv(){
         let imgDiv=document.createElement('div');
         let image=document.createElement('img');
         imgDiv.appendChild(image)
         image.src=this.img_link
-        return imgDiv
+        div.appendChild(imgDiv)
+
+        contentDiv.appendChild(div)
     }
+
 }
 
 
