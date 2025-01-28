b43d607080691c8e41b447bdf63a8c9257384727
teonteon19 <92378359+teonteon19@users.noreply.github.com>
2025-01-28 10:47:24 +0800

    add test.json at url

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 let test
 
-fetch("https://github.com/teonteon19/yusof_learning").then(response=>response.json()).then(result=>test=result)
+fetch("https://github.com/teonteon19/yusof_learning/test.json").then(response=>response.json()).then(result=>test=result)
