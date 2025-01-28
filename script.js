096e333e29955f27143c858536db00d0b9cc4415
teonteon19 <92378359+teonteon19@users.noreply.github.com>
2025-01-28 12:45:22 +0800

    modified script.js(define Subcontent class and add code to create content),create example.html,update thumbnails-image/main.json

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
-let test
+let content
+const contentDiv=document.querySelector('.content')
 
-let url="https://teonteon19.github.io/yusof_learning/test.json"
+let url="https://teonteon19.github.io/yusof_learning/thumbnails-image"
 
-fetch(url).then(response=>response.json()).then(result=>test=result)
+fetch(url+"/main.json").then(response=>response.json()).then(result=>{
+    content=result;
+    content.forEach(element => {
+        let subContent=new Subcontent(element.name,element.bahasa_melayu,url)
+        subContent.createSubcontentDiv(contentDiv)
+    });
+})
 
-if(!test){
-    test=2
+/*
+content is a list
+for each element in content create a div with 2 divs that will show the title and image
+
+*/
+
+
+class Subcontent{
+    constructor(name,bm,url){
+        this.name=name;
+        this.bm=bm;
+        this.img_link=url+this.name+".png"
+    }
+
+    //contentDiv refer to div.content
+    createSubcontentDiv(contentDiv){
+        let div=document.createElement('div');
+        div.className='thumbnail';
+        div.id=this.name;
+
+        let title=this.createTitleDiv();
+        div.appendChild(title)
+
+        let imgDiv=this.createImgDiv();
+        div.appendChild(this.createImgDiv)
+
+        contentDiv.appendChild(div)
+    }
+
+    createTitleDiv(){
+        let title=document.createElement('div');
+        title.innerText=this.name
+    }
+
+    createImgDiv(){
+        let imgDiv=document.createElement('div');
+        let image=document.createElement('img');
+        imgDiv.appendChild(image)
+        image.src=this.img_link
+    }
 }
+
+
