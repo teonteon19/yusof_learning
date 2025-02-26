cd3b65c719bedacb8141c00a834c05e8c702a76c
teonteon19 <92378359+teonteon19@users.noreply.github.com>
2025-02-26 16:35:49 +0800

    modified url

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let thumbnails
 const contentDiv=document.querySelector('.content')
 //import { Subcontent } from "./subcontent.js"
 
-let url="https://teonteon19.github.io/yusof_learning/thumbnails-image"//url address for gihub cdn
+let url="https://teonteon19.github.io/yusof_learning"//url address for gihub cdn
 //let url='http://127.0.0.1:5500'
 
 fetch(url+"/thumbnails-image/main.json").then(response=>response.json()).then(result=>{
